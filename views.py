4a3328417690f65a220dcd4794f259cba44c9c00
martins-collab <118398745+martins-collab@users.noreply.github.com>
2024-10-01 10:51:03 -0300

    Iniciando o projeto em Flask

---
+++ views.py
@@ -0,0 +1,16 @@
+from main import app
+from flask import render_template
+
+# rotas 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/cadastro.html")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/login.html")
+def login():
+    return render_template("login.html")
